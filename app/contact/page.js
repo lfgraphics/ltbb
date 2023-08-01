2fa0165fce8e4abc50f20e81a39077be37695877
Taha Kazmi <tahakazmi143@gmail.com>
2023-08-02 01:43:03 +0530

    started working on contact us form is not displaying

---
+++ app/contact/page.js
@@ -0,0 +1,15 @@
+"use client"
+import React from 'react';
+
+import Form from '@/components/contactus/Form';
+
+
+const page = () => {
+  return (
+  <React.Fragment>
+    <Form></Form>
+  </React.Fragment>
+  )
+}
+
+export default page;
