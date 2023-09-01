2655b760941599ceea1e1dca56270566ea6fcc4b
saadahmadhere <52596800+saadahmadhere@users.noreply.github.com>
2023-09-02 00:39:26 +0530

    [WIP]: remove coming soom page and add video to it

--- app/page.js
+++ app/page.js
@@ -1,12 +1,21 @@
-import Image from 'next/image'
-import React from 'react'
-import CommingSoon from '@/components/CommingSoon'
+import React from 'react';
+import CommingSoon from '@/components/CommingSoon';
 
 export default function Home() {
-  return (
-    <React.Fragment>
-      <CommingSoon></CommingSoon>
-      {/* <Landing></Landing> */}
-    </React.Fragment>
-  )
+	return (
+		<>
+			<video
+				autoPlay
+				muted
+				playsInline
+				loop
+				className='object-cover h-screen w-full'
+			>
+				<source
+					src='https://thelegalbase.com/wp-content/uploads/2022/07/My-Movie_Website.mp4'
+					type='video/mp4'
+				/>
+			</video>
+		</>
+	);
 }
