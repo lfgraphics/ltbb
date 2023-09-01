d2a68ee68caa019e6c4f195a279fe060cb36abf9
saadahmadhere <52596800+saadahmadhere@users.noreply.github.com>
2023-09-01 22:40:13 +0530

    [chore]: lint changes on faq and appstate.js

--- context/Appstate.js
+++ context/Appstate.js
@@ -1,17 +1,10 @@
-"use client"
-import React from "react";
-import { useState } from "react";
-import Appcontext from '../context/Appcontext'
+'use client';
+import React from 'react';
+import { useState } from 'react';
+import Appcontext from '../context/Appcontext';
 
-
-const Appstate = (props)=>{
-
-  
-    return (
-        <Appcontext.Provider value={{}}>
-  {props.children}
-        </Appcontext.Provider>
-    )
-}
+const Appstate = (props) => {
+	return <Appcontext.Provider value={{}}>{props.children}</Appcontext.Provider>;
+};
 
 export default Appstate;
