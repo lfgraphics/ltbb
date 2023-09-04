547afd236d75ee7f90f4d585b8bf15f9ade2aab0
saadahmadhere <52596800+saadahmadhere@users.noreply.github.com>
2023-09-04 22:02:58 +0530

    [feat]: add legal solution cards

---
+++ components/cards/Services/ServiceCard.jsx
@@ -0,0 +1,35 @@
+'use client';
+
+import { useState } from 'react';
+import Link from 'next/link';
+import { motion } from 'framer-motion';
+
+const ServiceCard = ({ route, title, hoverData, imageUrl }) => {
+	const [isHovered, setIsHovered] = useState(false);
+	console.log(imageUrl)
+	return (
+		<motion.div
+			className='w-52 h-72 hover:h-72 rounded-xl flex justify-center items-center shadow-slate-500 border-2 cursor-default bg-white text-center overflow-hidden'
+			onHoverStart={() => setIsHovered(true)}
+			onHoverEnd={() => setIsHovered(false)}
+		>
+
+			{isHovered ? (
+				<motion.p
+					initial={{ opacity: 0 }}
+					animate={{ opacity: isHovered ? 1 : 0 }}
+					transition={{ duration: 0.9 }}
+					className='text-sm text-start p-2'
+				>
+					{hoverData}
+				</motion.p>
+			) : (
+				<div className="h-full w-full flex align-center items-center bg-cover bg-opacity-50 bg-[url(/images/case-intake-card-bg.webp)]" >
+					<span className='text-lg p-2'>{title}</span>
+				</div>
+			)}
+		</motion.div>
+	);
+};
+
+export default ServiceCard;
---
+++ components/legalSolutions/index.jsx
@@ -0,0 +1,23 @@
+import { tlbServicesData } from '@/app/data/tlbServices';
+import ServiceCard from '../cards/Services/ServiceCard';
+
+const LegalSolutions = () => {
+	return (
+		<div className='flex flex-col items-center gap-12 p-8 mt-8'>
+			<span className='text-xl font-semibold tracking-wide'>LEGAL SOLUTIONS</span>
+			<div className='flex justify-center w-11/12 flex-wrap gap-2'>
+				{tlbServicesData.map((service) => (
+					<ServiceCard
+						key={service.title}
+						route={service.href}
+						hoverData={service.desc}
+						title={service.title}
+            imageUrl={service.imageUrl}
+					/>
+				))}
+			</div>
+		</div>
+	);
+};
+
+export default LegalSolutions;
