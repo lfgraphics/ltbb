2dad86feafcff97f5186995b2bb996de7a70ac36
saadahmadhere <52596800+saadahmadhere@users.noreply.github.com>
2023-09-05 20:30:26 +0530

    [feat]: optimise card images

--- app/data/tlbServices.js
+++ app/data/tlbServices.js
@@ -3,78 +3,78 @@ export const tlbServicesData = [
 		title: 'Case Intake Services',
 		desc: 'The Legal Base LPO provides high-quality legal intake solutions that are customised to your exact needs and requirements.',
 		href: '/caseintakeservices',
-		imageUrl: 'images/case-intake-card-bg.webp'
+		imageUrl: '/images/case-intake-card-bg.webp'
 	},
 	{
 		title: 'Contract Management',
 		desc: 'Contract lifecycle management (CLM) service refers to the management of contracts throughout their entire lifecycle, from creation and negotiation to execution, renewal, and termination.',
 		href: '/contractmanagement',
-		imageUrl: 'images/contract-management-card-bg.webp'
+		imageUrl: '/images/contract-management-card-bg.webp'
 	},
 	{
 		title: 'Deposition Summary Services',
 		desc: 'TLB\'s Deposition summary services are professional services that provide concise summaries of legal depositions.',
 		href: '/depositionsummaryservices',
-		imageUrl: 'images/deposition-summary-card-bg.webp'
+		imageUrl: '/images/deposition-summary-card-bg.webp'
 	},
 	{
 		title: 'Estate Planning Services',
 		desc: 'Estate planning services are professional services provided by attorneys, financial advisors, or estate planners to help individuals and families plan for the management and distribution of their assets upon death or incapacity.',
 		href: '/estate',
-		imageUrl: 'images/estate-planning-card-bg.webp'
+		imageUrl: '/images/estate-planning-card-bg.webp'
 	},
 	{
 		title: 'Family Law Services',
 		desc: 'Outsourced family law services refer to the practice of engaging external professionals or organizations to provide family law-related services on behalf of individuals or entities.',
 		href: '/familylawservices',
-		imageUrl: 'images/family-law-card-bg.webp'
+		imageUrl: '/images/family-law-card-bg.webp'
 	},
 	{
 		title: 'Global Immigration Management Services',
 		desc: 'Global Immigration Management Services refers to the comprehensive range of services provided by specialized companies or organizations to assist individuals, families, and businesses with their immigration and mobility needs on a global scale.',
 		href: '/globalimigration',
-		imageUrl: 'images/immigration-card-bg.webp'
+		imageUrl: '/images/immigration-card-bg.webp'
 	},
 	{
 		title: 'Global IP Research And Consultation Services',
 		desc: 'TLB\'s team, comprising of Patent Agents and Patent Attorneys, are highly trained and certified in a variety of technological fields and have excellent technical credentials as well as considerable legal and business expertise.',
 		href: '/href',
-		imageUrl: 'images/intellectual-property-card-bg.webp'
+		imageUrl: '/images/intellectual-property-card-bg.webp'
 	},
 	{
 		title: 'Legal Research Services',
 		desc: 'TLB\'s legal research outsourcing services help you make critical business choices, and we are dedicated to acting as an extension of your legal departments. Essentially, becoming a one-stop shop for all of your legal research needs.',
 		href: '/legalresearch',
-		imageUrl: 'images/legal-research-card-bg.webp'
+		imageUrl: '/images/legal-research-card-bg.webp'
 	},
 	{
 		title: 'Litigation Support',
 		desc: 'Our litigation support services can vary from assisting the attorneys, law firms, and general counsels in their day-to-day tasks.',
 		href: '/litigationsupport',
-		imageUrl: 'images/litigation-support-card-bg.webp'
+		imageUrl: '/images/litigation-support-card-bg.webp'
 	},
 	{
 		title: 'M&A Diligence Services',
 		desc: 'M&A diligence services can provide businesses with a deeper understanding of a potential acquisition target, enabling them to make more informed decisions about the deal and potentially negotiate more favourable terms.',
 		href: '/#',
-		imageUrl: 'images/m-and-a-card-bg.webp'
+		imageUrl: '/images/m-and-a-card-bg.webp'
 	},
 	{
 		title: 'Managed Document Review Services',
 		desc: 'A managed document review service is a professional service that assists law firms, corporations, or legal departments in reviewing large volumes of documents for litigation, regulatory compliance, investigations, or other legal purposes.',
 		href: '/manageddocumentreviewservice',
-		imageUrl: 'images/document-review-card-bg.webp'
+		imageUrl: '/images/document-review-card-bg.webp'
 	},
 	{
 		title: 'Paralegal Services',
 		desc: 'Paralegals, also known as legal assistants, are trained professionals who work under the supervision of lawyers to help them with legal work, research, and administrative tasks.',
 		href: '/paralegalservices',
-		imageUrl: 'images/paralegal-service-card-bg.webp'
+		imageUrl: '/images/paralegal-service-card-bg.webp'
 	},
 	{
 		title: 'Personal Injury Services',
 		desc: 'Outsourced personal injury support services involve engaging third-party providers like TLB to assist law firms and attorneys in handling various tasks and responsibilities related to personal injury cases.',
 		href: '/personalinjury',
-		imageUrl: 'images/personal-injury-card-bg.webp'
+		imageUrl: '/images/personal-injury-card-bg.webp'
 	},
 ];
--- components/cards/Services/ServiceCard.jsx
+++ components/cards/Services/ServiceCard.jsx
@@ -3,12 +3,13 @@
 import { useState } from 'react';
 import Link from 'next/link';
 import { motion } from 'framer-motion';
+import Image from 'next/image';
 
 const ServiceCard = ({ route, title, hoverData, url }) => {
 	const [isHovered, setIsHovered] = useState(false);
 	return (
 		<motion.div
-			className='w-52 h-72 hover:h-72 rounded-xl flex justify-center items-center shadow-slate-500 border-2 cursor-default bg-white text-center overflow-hidden'
+			className='w-52 h-72 relative rounded-xl flex justify-center items-center shadow-slate-500 border-2 cursor-default bg-white text-center overflow-hidden'
 			onHoverStart={() => setIsHovered(true)}
 			onHoverEnd={() => setIsHovered(false)}
 		>
@@ -18,14 +19,15 @@ const ServiceCard = ({ route, title, hoverData, url }) => {
 					initial={{ opacity: 0 }}
 					animate={{ opacity: isHovered ? 1 : 0 }}
 					transition={{ duration: 0.9 }}
-					className='text-sm text-start p-2'
+					className='text-md text-start p-2 leading-3'
 				>
 					{hoverData}
 				</motion.p>
 			) : (
-				<div className="h-full w-full flex align-center items-center bg-cover bg-opacity-50" style={{backgroundImage: `url(${url})`}}>
-					<span className='text-lg p-2'>{title}</span>
-				</div>
+				<>
+					<span className='text-lg font-semibold z-10'>{title}</span>
+					<Image src={url} fill alt="card background image" sizes="100%" style={{opacity: 0.4}}/>
+				</>
 			)}
 		</motion.div>
 	);
