0d0d6cb40da7c82af120607be35e9676e43319c9
saadahmadhere <52596800+saadahmadhere@users.noreply.github.com>
2023-09-07 21:30:45 +0530

    [feat]: add hover effects to card

--- components/cards/Services/ServiceCard.jsx
+++ components/cards/Services/ServiceCard.jsx
@@ -1,35 +1,32 @@
 'use client';
 
-import { useState } from 'react';
-import Link from 'next/link';
-import { motion } from 'framer-motion';
 import Image from 'next/image';
 
 const ServiceCard = ({ route, title, hoverData, url }) => {
-	const [isHovered, setIsHovered] = useState(false);
 	return (
-		<motion.div
-			className='w-52 h-72 relative rounded-xl flex justify-center items-center shadow-slate-500 border-2 cursor-default bg-white text-center overflow-hidden'
-			onHoverStart={() => setIsHovered(true)}
-			onHoverEnd={() => setIsHovered(false)}
-		>
-
-			{isHovered ? (
-				<motion.p
-					initial={{ opacity: 0 }}
-					animate={{ opacity: isHovered ? 1 : 0 }}
-					transition={{ duration: 0.9 }}
-					className='text-start p-2'
-				>
-					{hoverData}
-				</motion.p>
-			) : (
-				<>
-					<span className='text-lg font-semibold z-10 p-2'>{title}</span>
-					<Image src={url} preload fill alt="card background image" sizes="100%" style={{opacity: 0.4}}/>
-				</>
-			)}
-		</motion.div>
+			<div className='group h-72 w-72'>
+				<div className='relative h-full w-full rounded-xl shadow-xl transition-all duration-500 [transform-style:preserve-3d] duration-500 group-hover:[transform:rotateY(180deg)]'>
+					<div className='absolute inset-0 flex justify-center items-center'>
+						<span className='text-lg font-semibold p-2 text-center z-10'>
+							{title}
+						</span>
+						<Image
+							src={url}
+							alt='card background image'
+							className='h-full w-full rounded-xl object-cover'
+							priority
+							fill
+							sizes='100%'
+							style={{ opacity: 0.5 }}
+						/>
+					</div>
+					<div className='absolute inset-0 h-full w-full rounded-xl bg-black px-12 text-center text-slate-200 [transform:rotateY(180deg)] [backface-visibility:hidden]'>
+						<div className='flex min-h-full flex-col items-center justify-center'>
+							<div className='text-sm'>{hoverData}</div>
+						</div>
+					</div>
+				</div>
+			</div>
 	);
 };
 
--- components/legalSolutions/index.jsx
+++ components/legalSolutions/index.jsx
@@ -5,7 +5,7 @@ const LegalSolutions = () => {
 	return (
 		<div className='flex flex-col items-center gap-12 p-8 mt-8'>
 			<span className='text-xl font-semibold tracking-wide'>LEGAL SOLUTIONS</span>
-			<div className='flex justify-center w-11/12 flex-wrap gap-2'>
+			<div className='flex justify-center w-11/12 flex-wrap gap-4'>
 				{tlbServicesData.map((service) => (
 					<ServiceCard
 						key={service.title}
