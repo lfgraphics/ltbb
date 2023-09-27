774b228f4fae62bf2e59cebf4d01d8fc86834920
KaifKhan7393 <kaifayaz@student.iul.ac.in>
2023-09-27 13:27:46 +0530

    [Feat]: added the blogpost section

--- app/page.js
+++ app/page.js
@@ -1,21 +1,22 @@
-import React from 'react'
-import Banner from '@/components/home/Banner'
-import AnimatedShape from '@/components/service/AnimatedShape'
-import HomeAchievements from '@/components/home/HomeAchievements'
-import LegalSolutions from '@/components/home/LegalSolutions'
-import HowWeWork from '@/components/home/HowWeWork'
-import OurMissionAndVisions from '@/components/home/OurMissionAndVisions'
+import React from "react";
+import Banner from "@/components/home/Banner";
+import AnimatedShape from "@/components/service/AnimatedShape";
+import HomeAchievements from "@/components/home/HomeAchievements";
+import LegalSolutions from "@/components/home/LegalSolutions";
+import HowWeWork from "@/components/home/HowWeWork";
+import OurMissionAndVisions from "@/components/home/OurMissionAndVisions";
+import BlogSection from "@/components/home/BlogSection";
 
 export default function Home() {
   return (
-    <React.Fragment >
-      <Banner/>
-      <AnimatedShape background="#EFF2FF"/>
-      <HomeAchievements/>
-      <LegalSolutions/>
-      <HowWeWork/>
-      <OurMissionAndVisions/>
+    <React.Fragment>
+      <Banner />
+      <AnimatedShape background="#EFF2FF" />
+      <HomeAchievements />
+      <LegalSolutions />
+      <HowWeWork />
+      <OurMissionAndVisions />
+      <BlogSection/>
     </React.Fragment>
-  )
+  );
 }
- 
---
+++ components/home/BlogSection.jsx
@@ -0,0 +1,25 @@
+import { homeBlogData } from "@/contants/homeBlogData";
+import BlogCard from "./BlogCard";
+
+export default function BlogSection() {
+  return (
+    <section className="px-[5%] mb-10">
+      <div className="bg-homeBg flex flex-col py-10 items-center gap-4 text-white">
+        <div className="bg-gray-500 py-2 px-4 rounded-3xl">Our blog</div>
+        <h2 className="text-2xl md:text-3xl lg:text-4xl font-extrabold">
+          Resources and insights
+        </h2>
+        <p>
+          The latest industry news, interviews, technologies, and resources.
+        </p>
+      </div>
+      <div className="mt-10 flex flex-col md:flex-row gap-10 flex-wrap">
+       {
+        homeBlogData?.map((item)=>{
+            return <BlogCard key={item.id} data={item}/>
+        })
+       }
+      </div>
+    </section>
+  );
+}
