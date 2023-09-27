091e440e741e9f81f3104dcf66bf72332b8393f6
KaifKhan7393 <kaifayaz@student.iul.ac.in>
2023-09-27 13:57:14 +0530

    [Fix]: fix the image path

--- components/home/BlogSection.jsx
+++ components/home/BlogSection.jsx
@@ -4,7 +4,7 @@ import BlogCard from "./BlogCard";
 export default function BlogSection() {
   return (
     <section className="px-[5%] mb-10">
-      <div className="bg-homeBg flex flex-col py-10 items-center gap-4 text-white">
+      <div className="bg-homeBg flex flex-col py-10 items-center text-center gap-4 text-white">
         <div className="bg-gray-500 py-2 px-4 rounded-3xl">Our blog</div>
         <h2 className="text-2xl md:text-3xl lg:text-4xl font-extrabold">
           Resources and insights
