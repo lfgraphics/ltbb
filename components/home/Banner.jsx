3282d62926bff5883fba106ee62f9a30cd4deeee
saadahmadhere <52596800+saadahmadhere@users.noreply.github.com>
2023-09-28 10:58:21 +0530

    [WIP]: add animations in hero section

--- components/home/Banner.jsx
+++ components/home/Banner.jsx
@@ -1,12 +1,49 @@
 'use client';
+import { useEffect, useState } from 'react';
 import BannerContent from './BannerContent';
 import BannerVideo from './BannerVideo';
 export default function Banner() {
+  const [bannerIndex, setBannerIndex] = useState(3);
+	const bannerHeading = {
+		1: {
+			title: 'LEADING GLOBAL LPO',
+			content:
+				'The Legal Base LPO is a global outsourcing partner which works closely with law firms, attorneys and legal departments of corporations to deliver complex and innovative legal solutions.',
+		},
+    2: {
+      title: 'title 2',
+      content: 'content 2'
+    },
+    3: {
+      title: 'title 3',
+      content: 'content 3'
+    }
+	};
+
+  useEffect(() => {
+    let intervalID = setTimeout(() => {
+      console.log('here')
+      setBannerIndex(prev => {
+        if(prev === 3) return 1;
+        return prev+1
+      })
+    },2000);
+
+    return () => {
+      clearInterval(intervalID);
+    }
+  })
+
 	return (
-		<div className='min-h-[85vh] sm:min-h-[75vh] py-10 justify-center bg-homeBg homeClipPath relative flex items-center px-[5%]'>
-			<div className='flex flex-col-reverse sm:flex-row  gap-12 items-center h-full'>
-				<BannerContent />
+		<div className='min-h-[85vh] sm:min-h-[75vh] py-10 bg-homeBg homeClipPath px-[5%]'>
+			<div className='flex flex-col-reverse sm:flex-row gap-12 items-center h-full md:pt-10'>
+				<BannerContent content={bannerHeading[bannerIndex]}/>
 				<BannerVideo />
+			</div>
+			<div className='border-white border-2 flex justify-center gap-3'>
+				<div>1</div>
+				<div>2</div>
+				<div>3</div>
 			</div>
 		</div>
 	);
--- components/home/BannerContent.jsx
+++ components/home/BannerContent.jsx
@@ -1,16 +1,15 @@
 import React from "react";
 import Link from "next/link";
 
-export default function BannerContent() {
+export default function BannerContent({content}) {
+  const {title, content: bannerContent} = content;
   return (
     <div className="flex flex-1 w-full flex-col gap-3 text-white">
       <h1 className="text-2xl md:text-3xl lg:text-4xl font-bold">
-        LEADING GLOBAL LPO
+        {title}
       </h1>
       <p className="text-gray-300">
-        The Legal Base LPO is a global outsourcing partner which works closely
-        with law firms, attorneys and legal departments of corporations to
-        deliver complex and innovative legal solutions.
+        {bannerContent}
       </p>
       <Link
         href="#"
