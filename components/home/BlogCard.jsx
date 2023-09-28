c9aa8ad871dce75de66eb2cc4ed4edc5e2fe269b
saadahmadhere <52596800+saadahmadhere@users.noreply.github.com>
2023-09-28 10:31:14 +0530

    [fix]: blog card width

--- components/home/BlogCard.jsx
+++ components/home/BlogCard.jsx
@@ -5,10 +5,10 @@ export default function BlogCard({ data }) {
   const { image, title, desc, link } = data;
   console.log(title);
   return (
-    <div className="flex-1 min-w-[300px] md:min-w-[350px]  p-4 bg-white drop-shadow-md">
+    <div className="flex-1 min-w-[300px] md:max-w-[320px] rounded-md p-4 bg-white drop-shadow-md">
       <div className="relative w-full h-56">
         <Link href={link} className="absolute z-10 inset-0"></Link>
-        <Image src={image} fill alt={title} className="object-cover"/>
+        <Image src={image} fill alt={title} className="object-cover rounded-md"/>
       </div>
       <div className="text-black mt-4 flex flex-col gap-3">
         <h3 className="text-2xl md:text-2xl line-clamp-1 hover:underline underline-offset-4 transition duration-200">
--- components/home/BlogSection.jsx
+++ components/home/BlogSection.jsx
@@ -3,7 +3,7 @@ import BlogCard from "./BlogCard";
 
 export default function BlogSection() {
   return (
-    <section className=" mb-10">
+    <section className="mb-10">
       <div className="bg-homeBg px-[5%] flex flex-col py-10 items-center text-center gap-4 text-white">
         <div className="bg-gray-500 py-2 px-4 rounded-3xl">Our blog</div>
         <h2 className="text-2xl md:text-3xl lg:text-4xl font-extrabold">
