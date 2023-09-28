4927ba8a3a59066f160c94a545b136d3d2e80545
saadahmadhere <52596800+saadahmadhere@users.noreply.github.com>
2023-09-28 10:37:28 +0530

    [refactor]: banner css

--- components/home/Banner.jsx
+++ components/home/Banner.jsx
@@ -1,15 +1,13 @@
-"use client";
-import BannerContent from "./BannerContent";
-import BannerVideo from "./BannerVideo";
+'use client';
+import BannerContent from './BannerContent';
+import BannerVideo from './BannerVideo';
 export default function Banner() {
-  return (
-    <div className="min-h-[85vh] sm:min-h-[75vh] py-10 justify-center bg-homeBg homeClipPath relative flex items-center px-[5%]">
-      <div className="flex flex-col-reverse sm:flex-row  gap-12 items-center h-full">
-        <BannerContent />
-        <div className="flex-1 w-full h-full max-h-80">
-          <BannerVideo />
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div className='min-h-[85vh] sm:min-h-[75vh] py-10 justify-center bg-homeBg homeClipPath relative flex items-center px-[5%]'>
+			<div className='flex flex-col-reverse sm:flex-row  gap-12 items-center h-full'>
+				<BannerContent />
+				<BannerVideo />
+			</div>
+		</div>
+	);
 }
--- components/home/BannerVideo.jsx
+++ components/home/BannerVideo.jsx
@@ -1,19 +1,20 @@
 export default function BannerVideo() {
-
-  return (
-    <div className="border-2 mx-auto rounded-lg min-h-[250px] md:min-h-[300px] w-full max-w-xs md:max-w-md border-gray-400 h-full relative">
-      <div className="absolute h-full w-full flex justify-center items-center">
-        <video
-          className="w-full h-full relative object-center object-cover rounded-lg -left-3 -top-3"
-          loop
-          autoPlay
-        >
-          <source
-            src="https://thelegalbase.com/wp-content/uploads/2022/07/My-Movie_Website.mp4"
-            type="video/mp4"
-          />
-        </video>
-      </div>
-    </div>
-  );
+	return (
+		<div className='flex-1 w-full h-full max-h-80'>
+			<div className='border-2 mx-auto rounded-lg min-h-[250px] md:min-h-[300px] w-full max-w-xs md:max-w-md border-gray-400 h-full relative'>
+				<div className='absolute h-full w-full flex justify-center items-center'>
+					<video
+						className='w-full h-full relative object-center object-cover rounded-lg -left-3 -top-3'
+						loop
+						autoPlay
+					>
+						<source
+							src='https://thelegalbase.com/wp-content/uploads/2022/07/My-Movie_Website.mp4'
+							type='video/mp4'
+						/>
+					</video>
+				</div>
+			</div>
+		</div>
+	);
 }
