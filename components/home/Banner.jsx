eff9f2071ada4877dbc018105334ed5ffa7f923d
saadahmadhere <52596800+saadahmadhere@users.noreply.github.com>
2023-10-01 14:54:27 +0530

    [feat]: add transition in hero text

--- components/home/Banner.jsx
+++ components/home/Banner.jsx
@@ -16,31 +16,32 @@ export default function Banner() {
     },
     [inViewRef]
   );
+  
+  const handleContentChange = (index) => {
+    setBannerIndex(index + 1);
+  };
+
   useEffect(() => {
     let intervalID;
     if (inView) {
       intervalID = setInterval(() => {
         setBannerIndex((prev) => (prev === 3 ? 1 : prev + 1));
-      }, 2000);
+      }, 4000);
     }
     return () => {
       clearInterval(intervalID);
     };
   }, [inView, bannerIndex]);
 
-  const handleContentChange = (index) => {
-    setBannerIndex(index + 1);
-  };
-
   return (
-    <div className="min-h-[85vh] sm:min-h-[75vh] py-10 flex flex-col justify-between bg-homeBg homeClipPath px-[5%]">
+    <div className="lg:min-h-[85vh] md:min-h-[60vh] sm:min-h-[75vh] py-10 flex flex-col justify-between bg-homeBg homeClipPath px-[5%]">
       <div className="flex flex-col-reverse sm:flex-row gap-12 items-center h-full md:pt-10">
         <div className="flex-1" ref={setRefs}>
           <BannerContent content={bannerHeading[bannerIndex]} />
         </div>
         <BannerVideo />
       </div>
-      <div className="flex md:mb-8 justify-center gap-3">
+      <div className="flex mb-0 md:mb-8 justify-center gap-3 mt-10 lg:mt-0">
         {Array(3)
           .fill(1)
           .map((_, i) => {
@@ -48,7 +49,7 @@ export default function Banner() {
               <button
                 key={i}
                 onClick={() => handleContentChange(i)}
-                className={`w-4 h-4 rounded-full ${
+                className={`md:w-3 md:h-3 md:rounded-full w-2 h-2 rounded-lg ${
                   i === bannerIndex - 1 ? "bg-homeCTA" : "bg-gray-300"
                 }`}
               ></button>
--- components/home/BannerContent.jsx
+++ components/home/BannerContent.jsx
@@ -1,16 +1,35 @@
-import React from "react";
+import { motion } from "framer-motion";
 import Link from "next/link";
 
 export default function BannerContent({content}) {
   const {title, content: bannerContent,link} = content;
+  const variants = {
+    show: {
+      opacity: 1,
+      transition: {
+        ease: "easeOut",
+        duration: 2
+      }
+    },
+    hide: {
+      opacity: 0
+    }
+  };
   return (
     <div className="flex flex-1 w-full flex-col gap-3 text-white">
-      <h1 className="text-2xl md:text-3xl lg:text-4xl font-bold">
-        {title}
-      </h1>
-      <p className="text-gray-300">
-        {bannerContent}
-      </p>
+      <motion.div
+        key={title}
+        variants={variants}
+        animate={"show"}
+        initial="hide"
+      >
+        <h1 className="text-2xl md:text-3xl lg:text-4xl font-bold">
+          {title}
+        </h1>
+        <p className="text-gray-300 h-36 md:h-20 mt-4">
+          {bannerContent}
+        </p>
+      </motion.div>
       <Link
         href={link}
         className="bg-homeCTA rounded-xl py-2 px-6 hover:opacity-90 max-w-fit"
