bb9f1f10d45228a8027e0df5644231ced8002e08
KaifKhan7393 <kaifayaz@student.iul.ac.in>
2023-10-01 10:23:59 +0530

    [Feat]: clear the interval when banner is not visible

--- components/home/Banner.jsx
+++ components/home/Banner.jsx
@@ -1,23 +1,32 @@
 "use client";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef, useCallback } from "react";
+import { useInView } from "react-intersection-observer";
 import BannerContent from "./BannerContent";
 import BannerVideo from "./BannerVideo";
 import { bannerHeading } from "@/constants/bannerContentData";
 export default function Banner() {
   const [bannerIndex, setBannerIndex] = useState(1);
+  const bannerRef = useRef();
+  const { ref: inViewRef, inView } = useInView();
 
+  const setRefs = useCallback(
+    (node) => {
+      bannerRef.current = node;
+      inViewRef(node);
+    },
+    [inViewRef]
+  );
   useEffect(() => {
-    let intervalID = setTimeout(() => {
-      setBannerIndex((prev) => {
-        if (prev === 3) return 1;
-        return prev + 1;
-      });
-    }, 2000);
-
+    let intervalID;
+    if (inView) {
+      intervalID = setInterval(() => {
+        setBannerIndex((prev) => (prev === 3 ? 1 : prev + 1));
+      }, 2000);
+    }
     return () => {
       clearInterval(intervalID);
     };
-  }, [bannerIndex]);
+  }, [inView, bannerIndex]);
 
   const handleContentChange = (index) => {
     setBannerIndex(index + 1);
@@ -26,7 +35,9 @@ export default function Banner() {
   return (
     <div className="min-h-[85vh] sm:min-h-[75vh] py-10 flex flex-col justify-between bg-homeBg homeClipPath px-[5%]">
       <div className="flex flex-col-reverse sm:flex-row gap-12 items-center h-full md:pt-10">
-        <BannerContent content={bannerHeading[bannerIndex]} />
+        <div className="flex-1" ref={setRefs}>
+          <BannerContent content={bannerHeading[bannerIndex]} />
+        </div>
         <BannerVideo />
       </div>
       <div className="flex md:mb-8 justify-center gap-3">
