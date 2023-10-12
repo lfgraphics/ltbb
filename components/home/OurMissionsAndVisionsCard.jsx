691f884c98052b6f5e90a54a425d55ab79967ed2
saadahmadhere <52596800+saadahmadhere@users.noreply.github.com>
2023-10-13 01:06:47 +0530

    [fix]: box shadow in OurMissionsAndVisionsCard

--- components/home/OurMissionsAndVisionsCard.jsx
+++ components/home/OurMissionsAndVisionsCard.jsx
@@ -1,8 +1,15 @@
-export default function OurMissionsAndVisionsCard({ title, desc,animationType }) {
-  return (
-    <div className="flex-1 flex flex-col gap-2 p-4 bg-white drop-shadow-md rounded-md" data-aos={animationType}>
-      <h2 className="text-xl md:text-2xl lg:text-3xl">{title}</h2>
-      <p>{desc}</p>
-    </div>
-  );
+export default function OurMissionsAndVisionsCard({
+	title,
+	desc,
+	animationType,
+}) {
+	return (
+		<div
+			className='flex-1 flex flex-col gap-2 p-4 bg-white shadow-3xl rounded-lg'
+			data-aos={animationType}
+		>
+			<h2 className='text-xl md:text-2xl lg:text-3xl'>{title}</h2>
+			<p>{desc}</p>
+		</div>
+	);
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: 'class',
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-      colors:{
-        'homeBg':'#F0F3FA',
-        'homeCTA':'#4E6BFF'
-      }
-    },
-  },
-  plugins: [],
-  
-}
+	darkMode: 'class',
+	content: [
+		'./pages/**/*.{js,ts,jsx,tsx,mdx}',
+		'./components/**/*.{js,ts,jsx,tsx,mdx}',
+		'./app/**/*.{js,ts,jsx,tsx,mdx}',
+	],
+	theme: {
+		extend: {
+			boxShadow: {
+				'3xl': '0 35px 60px -15px rgba(0, 0, 0, 0.3)',
+			},
+			backgroundImage: {
+				'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+				'gradient-conic':
+					'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+			},
+			colors: {
+				homeBg: '#F0F3FA',
+				homeCTA: '#4E6BFF',
+			},
+		},
+	},
+	plugins: [],
+};
