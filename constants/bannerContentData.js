757d2e17b5948eaed371243c77c210373890a3aa
saadahmadhere <52596800+saadahmadhere@users.noreply.github.com>
2023-10-14 12:36:36 +0530

    [fix]: minor fixes

--- constants/bannerContentData.js
+++ constants/bannerContentData.js
@@ -1,26 +1,26 @@
 export const bannerHeading = {
-  1: {
-    title: "TLB - A global LOOP(Legal Operations Outsourcing Partner)",
-    content:
-      "TLB is a global legal process outsourcing partner formed in 2019 with expertise in working with independent attorneys, law firms and corporate legal departments based out of US, UK and Canada.",
-    link: "#contact"
-  },
-  2: {
-    title: "TLB's Legal Outsourced Services",
-    content:
-      "TLB's Legal Services team consists of legal experts trained in US, UK and Canadian laws and with rich experience of providing outsourced legal operations support to independent attorneys, law firms and corporate legal departments.",
-    link: "#contact"
-  },
-  3: {
-    title: "TLB's Managed Outsourced Services",
-    content:
-      "TLB's Managed Services team has unique experience of supporting independent attorneys, law firms and corporate legal departments based out of US, UK and Canada to provide them with value add managed services like website development, digital marketing and bespoke IT and app development.",
-    link: "#contact"
-  },
-  4: {
-    title: "Founded and Operated by Legal Industry Experts",
-    content: "Founded in 2019, TLB has been formed with a vision to establish itself as a trusted outsourced partner for independent attorneys, law firms and corporate legal departments based out of US, UK and Canada.",
-    link: "#contact"
-  }
+	1: {
+		title: 'TLB - A global LOOP\n (Legal Operations Outsourcing Partner)',
+		content:
+			'TLB is a global legal process outsourcing partner formed in 2019 with expertise in working with independent attorneys, law firms and corporate legal departments based out of US, UK and Canada.',
+		link: '#contact',
+	},
+	2: {
+		title: "TLB's Legal Outsourced Services",
+		content:
+			"TLB's Legal Services team consists of legal experts trained in US, UK and Canadian laws and with rich experience of providing outsourced legal operations support to independent attorneys, law firms and corporate legal departments.",
+		link: '#contact',
+	},
+	3: {
+		title: "TLB's Managed Outsourced Services",
+		content:
+			"TLB's Managed Services team has unique experience of supporting independent attorneys, law firms and corporate legal departments based out of US, UK and Canada to provide them with value add managed services like website development, digital marketing and bespoke IT and app development.",
+		link: '#contact',
+	},
+	4: {
+		title: 'Founded and Operated by Legal Industry Experts',
+		content:
+			'Founded in 2019, TLB has been formed with a vision to establish itself as a trusted outsourced partner for independent attorneys, law firms and corporate legal departments based out of US, UK and Canada.',
+		link: '#contact',
+	},
 };
-
