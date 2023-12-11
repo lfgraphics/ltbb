8cb01e14781e7889a57acd6328117da063aa06e0
kaifayazkhan <kaifayazkhan@gmail.com>
2023-12-12 01:16:20 +0530

    [fix]: fix

--- components/manageddocumentreviewservice/Four.js
+++ components/manageddocumentreviewservice/Four.js
@@ -1,12 +1,7 @@
 'use client'
 import React, { useEffect } from 'react';
 import AOS from 'aos';
-
-import 'aos/dist/aos.css';
-import Image from 'next/image';
-
 import "aos/dist/aos.css";
-import Image from "next/image";
 
 const Four = () => {
   useEffect(() => {
