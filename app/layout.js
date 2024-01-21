a0fad0bbeaa1001548060f8337c2eb1312df764a
kaifayazkhan <kaifayazkhan@gmail.com>
2024-01-22 01:16:44 +0530

    [update]: og link

--- app/layout.js
+++ app/layout.js
@@ -7,13 +7,13 @@ import Footer from '@/components/Footer';
 const inter = Varela({ subsets: ['latin'], weight: '400' })
 
 export const metadata = {
-  metadataBase: new URL("https://thelegalbase.com"),
+  metadataBase: new URL("https://tlb-ten.vercel.app/"),
   title: 'TLB',
   description: 'TLB LPO is a global outsourcing partner which works closely with law firms , attorneys and legal departments of corporations',
   openGraph: {
     title: "TLB",
     type: 'website',
-    url: 'https://tlb-ten.vercel.app',
+    url: 'https://tlb-ten.vercel.app/',
     description: 'TLB LPO is a global outsourcing partner which works closely with law firms , attorneys and legal departments of corporations',
     images: {
       type: "image/png",
